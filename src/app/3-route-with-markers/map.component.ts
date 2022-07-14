342541b3b05ee502ed9312ab43d0687b61411566
anartzdev <amugika@atsistemas.com>
2022-07-14 07:33:39 +0200

    2 files modified

--- src/app/3-route-with-markers/map.component.ts
+++ src/app/3-route-with-markers/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IConfigMap, IMarker } from '@mugan86/ng-leaflet';
 import gpxParser from 'gpxparser';
 
